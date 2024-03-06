42aed34d987990611718d1fcddf403ac25bc35ab
agschro2 <agschro2@ncsu.edu>
2024-03-06 11:17:51 -0500

    [Initial Commit] Generate Project

---
+++ PixelPhysics/html/src/com/aidenx11/game/client/HtmlLauncher.java
@@ -0,0 +1,22 @@
+package com.aidenx11.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.aidenx11.game.pixelPhysicsGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                // Resizable application, uses available space in browser
+                return new GwtApplicationConfiguration(true);
+                // Fixed size application:
+                //return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new pixelPhysicsGame();
+        }
+}
