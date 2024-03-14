6e1d3bbfe1f22b4fe0acec6726bb7724db6c3c63
aidenx11 <giovanni523@outlook.com>
2024-03-13 21:46:16 -0400

    [Implementation] Added square brush

--- PixelPhysics/core/src/com/aidenx11/game/pixelPhysicsGame.java
+++ PixelPhysics/core/src/com/aidenx11/game/pixelPhysicsGame.java
@@ -1,11 +1,7 @@
 package com.aidenx11.game;
 
-import com.aidenx11.game.color.ColorValues;
-import com.aidenx11.game.color.CustomColor;
-import com.aidenx11.game.elements.Sand;
 import com.aidenx11.game.input.MouseInput;
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -14,7 +10,7 @@ public class pixelPhysicsGame extends ApplicationAdapter {
 
 	public static int screenWidth = 1024; // MUST equal window size in desktop launcher
 	public static int screenHeight = 768; // MUST equal window size in desktop launcher
-	public static int pixelSizeModifier = 20;
+	public static int pixelSizeModifier = 10;
 	public CellularMatrix matrix;
 
 	public static int rows;
@@ -35,27 +31,25 @@ public class pixelPhysicsGame extends ApplicationAdapter {
 		shapeRenderer.setProjectionMatrix(camera.combined);
 		shapeRenderer.setAutoShapeType(true);
 
-		int fittedHeight = screenHeight - (screenHeight % pixelSizeModifier);
-		int fittedWidth = screenWidth - (screenWidth % pixelSizeModifier);
-
-		rows = fittedHeight / pixelSizeModifier;
-		columns = fittedWidth / pixelSizeModifier;
+		rows = (int) Math.ceil(screenHeight / pixelSizeModifier) + 1;
+		columns = (int) Math.ceil(screenWidth / pixelSizeModifier) + 1;
 
 		matrix = new CellularMatrix(rows, columns, pixelSizeModifier);
 
 		mouse = new MouseInput(matrix, camera);
-		
 
 	}
 
 	@Override
 	public void render() {
 		
+		// Set blue background
 		ScreenUtils.clear(135/255f, 206/255f, 235/255f, 1);
 
 		// Detects mouse input and sets pixel if it is in bounds
 		mouse.detectInput();
 		
+		// Perform sand settling logic
 		matrix.updateFrame(shapeRenderer);
 
 	}
