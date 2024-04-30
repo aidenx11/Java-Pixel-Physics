9fb973764f5ba44e89c39fffe21849e69243850d
aidenx11 <giovanni523@outlook.com>
2024-04-29 21:05:44 -0400

    [HTML]
    Changed HTML build to use window size instead of fixed size

--- JavaPixelPhysics/html/src/main/java/com/aidenx11/JavaPixelPhysics/gwt/GwtLauncher.java
+++ JavaPixelPhysics/html/src/main/java/com/aidenx11/JavaPixelPhysics/gwt/GwtLauncher.java
@@ -10,13 +10,13 @@ public class GwtLauncher extends GwtApplication {
         @Override
         public GwtApplicationConfiguration getConfig () {
             // Resizable application, uses available space in browser with no padding:
-//            GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(GwtApplication.isMobileDevice());
-//            cfg.padVertical = 0;
-//            cfg.padHorizontal = 0;
-//            return cfg;
+            GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(GwtApplication.isMobileDevice());
+            cfg.padVertical = 0;
+            cfg.padHorizontal = 0;
+            return cfg;
             // If you want a fixed size application, comment out the above resizable section,
             // and uncomment below:
-            return new GwtApplicationConfiguration(1280, 720, GwtApplication.isMobileDevice());
+//            return new GwtApplicationConfiguration(1280, 720, GwtApplication.isMobileDevice());
         }
 
         @Override
